8356553a64a935e5a44fd78ad6fbac215a4c1875
Kronos-XR <nikitka-solova@mail.ru>
2023-12-26 01:07:43 +0300

    Made mvvm navigation for example with https://github.com/homoluden/MVVM-Navigation

---
+++ Services/Interfaces/IPageResolver.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace Textile.Services.Interfaces
+{
+    public interface IPageResolver
+    {
+        Page GetPageInstance(string alias);
+    }
+}
---
+++ ViewModels/ViewModelsResolver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Textile.ViewModels.Interfaces;
+
+namespace Textile.ViewModels
+{
+    public class ViewModelsResolver : IViewModelsResolver
+    {
+
+        private readonly Dictionary<string, Func<INotifyPropertyChanged>> _vmResolvers = new Dictionary<string, Func<INotifyPropertyChanged>>();
+
+        public ViewModelsResolver()
+        {
+            // TODO: Add Pages for navigation
+            _vmResolvers.Add(MainViewModel.Page1ViewModelAlias, () => new Page1ViewModel());
+            _vmResolvers.Add(MainViewModel.NotFoundPageViewModelAlias, () => new Page404ViewModel());
+        }
+
+        public INotifyPropertyChanged GetViewModelInstance(string alias)
+        {
+            if (_vmResolvers.ContainsKey(alias))
+            {
+                return _vmResolvers[alias]();
+            }
+
+            return _vmResolvers[MainViewModel.NotFoundPageViewModelAlias]();
+        }
+    }
+}
