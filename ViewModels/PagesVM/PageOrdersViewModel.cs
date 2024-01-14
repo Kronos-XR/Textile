73957f76cc87b4afeb8e0c37a8af49b03e822974
Kronos-XR <nikitka-solova@mail.ru>
2024-01-14 22:20:08 +0300

    add page bindings

--- ViewModels/PagesVM/PageOrdersViewModel.cs
+++ ViewModels/PagesVM/PageOrdersViewModel.cs
@@ -8,5 +8,9 @@ namespace Textile.ViewModels
 {
     public class PageOrdersViewModel : BaseViewModel
     {
+        public string PageOrdersText
+        {
+            get { return "Hello, Orders!\nSent from my OrdersPage Orders..."; }
+        }
     }
 }
