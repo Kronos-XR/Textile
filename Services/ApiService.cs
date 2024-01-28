ac4f52fc832fa085c1bcf95d9bddec4cb8a4c10d
Kronos-XR <nikitka-solova@mail.ru>
2024-01-28 22:52:32 +0300

    Created task blanks for the api and changed the GetProductPage interface

--- Services/ApiService.cs
+++ Services/ApiService.cs
@@ -13,80 +13,78 @@ namespace Textile.Services
     //TODO: Разделить адреса api от функционала, если бекенд переедет то нужно будет чинить каждую ссылку 
     internal class ApiService
     {
-        #region GetProduct
-        public static async Task<Product> GetDataProductFromApiAsync(int productId)
+        private readonly HttpClient _httpClient;
+        public ApiService()
         {
-            string apiUrl = $"http://www.diplomapi.somee.com/api/products/{productId}";
-            using (HttpClient client = new HttpClient())
-            {
-                try
-                {
-                    HttpResponseMessage response = await client.GetAsync(apiUrl);
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        string responseData = await response.Content.ReadAsStringAsync();
-                        Product product = JsonConvert.DeserializeObject<Product>(responseData);
-                        return product;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Ошибка: {response.StatusCode} - {response.ReasonPhrase}");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Произошла ошибка: {ex.Message}");
-                }
-                return null;
-            }
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new System.Uri("http://www.diplomapi.somee.com");
         }
 
-        public static async Task GetProductAsync(int productId = 1)
+        private async Task<List<Product>> GetProductsInApiAsync()
         {
-            Product product = await GetDataProductFromApiAsync(productId);
-        }
-        #endregion
+            HttpResponseMessage response = await _httpClient.GetAsync("/api/products");
+            response.EnsureSuccessStatusCode();
 
-        #region Put
-        public static async Task<Product> UpdateDataProductToApiAsync(int productId, Product updatedProduct)
-        {
-            string apiUrl = $"http://www.diplomapi.somee.com/api/products/{productId}";
+            string responseBody = await response.Content.ReadAsStringAsync();
+            List<Product> products = JsonConvert.DeserializeObject<List<Product>>(responseBody);
 
-            using (HttpClient client = new HttpClient())
-            {
-                try
-                {
-                    // Сериализация объекта updatedProduct в JSON и отправка в теле запроса
-                    string jsonBody = JsonConvert.SerializeObject(updatedProduct);
-                    HttpContent content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
-
-                    // Выполнение PUT запроса
-                    HttpResponseMessage response = await client.PutAsync(apiUrl, content);
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        string responseData = await response.Content.ReadAsStringAsync();
-                        Product product = JsonConvert.DeserializeObject<Product>(responseData);
-                        return product;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Ошибка: {response.StatusCode} - {response.ReasonPhrase}");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Произошла ошибка: {ex.Message}");
-                }
-                return null;
-            }
+            return products;
         }
 
-        public static async Task UpdateProductAsync(int productId, Product updatedProduct)
+        private async Task<Product> GetProductByIdInApiAsync(int productId)
         {
-            Product product = await UpdateDataProductToApiAsync(productId, updatedProduct);
+            HttpResponseMessage response = await _httpClient.GetAsync($"/api/products/{productId}");
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Product product = JsonConvert.DeserializeObject<Product>(responseBody);
+
+            return product;
         }
-        #endregion
+        private async Task<int> PostProductInApiAsync(Product product)
+        {
+            string productJson = JsonConvert.SerializeObject(product);
+            StringContent content = new StringContent(productJson, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _httpClient.PostAsync("/api/products", content);
+            response.EnsureSuccessStatusCode();
+
+            string createdProductJson = await response.Content.ReadAsStringAsync();
+            Product createdProduct = JsonConvert.DeserializeObject<Product>(createdProductJson);
+
+            return createdProduct.Id;
+        }
+
+        private async Task<HttpResponseMessage> PutProductInApiAsync(Product product)
+        {
+            string productJson = JsonConvert.SerializeObject(product);
+            StringContent content = new StringContent(productJson, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _httpClient.PutAsync($"/api/products/{product.Id}", content);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        private async Task<HttpResponseMessage> DeleteProductInApiAsync(int productId)
+        {
+            HttpResponseMessage response = await _httpClient.DeleteAsync($"/api/products/{productId}");
+            return response.EnsureSuccessStatusCode();
+        }
+
+        public async Task GetProductAsync(int productId)
+        {
+            Product product = await GetProductByIdInApiAsync(productId);
+        }
+        public async Task CreateProductAsync(Product product)
+        {
+            await PostProductInApiAsync(product);
+        }
+        public async Task UpdateProductAsync(Product product)
+        {
+            await PutProductInApiAsync(product);
+        }
+        public async Task DeleteProductAsync(int productId)
+        {
+            await DeleteProductInApiAsync(productId);
+        }
     }
 }
