4c629f77657f8ebf075e2c71d49e9a00cbac65a1
Kronos-XR <nikitka-solova@mail.ru>
2024-01-28 23:30:09 +0300

    Add error handlers for all api tasks

--- Services/ApiService.cs
+++ Services/ApiService.cs
@@ -13,63 +13,130 @@ namespace Textile.Services
     //TODO: Разделить адреса api от функционала, если бекенд переедет то нужно будет чинить каждую ссылку 
     internal class ApiService
     {
+        #region HttpClient Configuration
+
         private readonly HttpClient _httpClient;
         public ApiService()
         {
             _httpClient = new HttpClient();
             _httpClient.BaseAddress = new System.Uri("http://www.diplomapi.somee.com");
         }
+        #endregion
 
+        #region GetProductsInApiAsync
         private async Task<List<Product>> GetProductsInApiAsync()
         {
-            HttpResponseMessage response = await _httpClient.GetAsync("/api/products");
-            response.EnsureSuccessStatusCode();
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync("/api/products");
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    List<Product> products = JsonConvert.DeserializeObject<List<Product>>(responseBody);
 
-            string responseBody = await response.Content.ReadAsStringAsync();
-            List<Product> products = JsonConvert.DeserializeObject<List<Product>>(responseBody);
-
-            return products;
+                    return products;
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+            return null;
         }
+        #endregion
 
+        #region GetProductByIdInApiAsync
         private async Task<Product> GetProductByIdInApiAsync(int productId)
         {
-            HttpResponseMessage response = await _httpClient.GetAsync($"/api/products/{productId}");
-            response.EnsureSuccessStatusCode();
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync($"/api/products/{productId}");
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Product product = JsonConvert.DeserializeObject<Product>(responseBody);
 
-            string responseBody = await response.Content.ReadAsStringAsync();
-            Product product = JsonConvert.DeserializeObject<Product>(responseBody);
-
-            return product;
+                    return product;
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+            return null;
         }
+        #endregion
+
+        #region PostProductInApiAsync
         private async Task<int> PostProductInApiAsync(Product product)
         {
-            string productJson = JsonConvert.SerializeObject(product);
-            StringContent content = new StringContent(productJson, Encoding.UTF8, "application/json");
+            try
+            {
+                string productJson = JsonConvert.SerializeObject(product);
+                StringContent content = new StringContent(productJson, Encoding.UTF8, "application/json");
 
-            HttpResponseMessage response = await _httpClient.PostAsync("/api/products", content);
-            response.EnsureSuccessStatusCode();
+                HttpResponseMessage response = await _httpClient.PostAsync("/api/products", content);
+                response.EnsureSuccessStatusCode();
 
-            string createdProductJson = await response.Content.ReadAsStringAsync();
-            Product createdProduct = JsonConvert.DeserializeObject<Product>(createdProductJson);
+                string createdProductJson = await response.Content.ReadAsStringAsync();
+                Product createdProduct = JsonConvert.DeserializeObject<Product>(createdProductJson);
 
-            return createdProduct.Id;
+                return createdProduct.Id;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+            return 0;
         }
+        #endregion
 
+        #region PutProductInApiAsync
         private async Task<HttpResponseMessage> PutProductInApiAsync(Product product)
         {
-            string productJson = JsonConvert.SerializeObject(product);
-            StringContent content = new StringContent(productJson, Encoding.UTF8, "application/json");
+            try
+            {
+                string productJson = JsonConvert.SerializeObject(product);
+                StringContent content = new StringContent(productJson, Encoding.UTF8, "application/json");
 
-            HttpResponseMessage response = await _httpClient.PutAsync($"/api/products/{product.Id}", content);
-            return response.EnsureSuccessStatusCode();
+                HttpResponseMessage response = await _httpClient.PutAsync($"/api/products/{product.Id}", content);
+                return response.EnsureSuccessStatusCode();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
         }
+        #endregion
 
+        #region DeleteProductInApiAsync
         private async Task<HttpResponseMessage> DeleteProductInApiAsync(int productId)
         {
-            HttpResponseMessage response = await _httpClient.DeleteAsync($"/api/products/{productId}");
-            return response.EnsureSuccessStatusCode();
+            try
+            {
+                HttpResponseMessage response = await _httpClient.DeleteAsync($"/api/products/{productId}");
+                return response.EnsureSuccessStatusCode();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
         }
+        #endregion 
 
+        #region Tasks for page
         public async Task GetProductAsync(int productId)
         {
             Product product = await GetProductByIdInApiAsync(productId);
@@ -86,5 +153,6 @@ namespace Textile.Services
         {
             await DeleteProductInApiAsync(productId);
         }
+        #endregion
     }
 }
